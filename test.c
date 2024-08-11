aa225b70267d60a9a2c7e7ea640e9a45250648f5
hotate <hiromsa@gmail.com>
2024-08-11 16:58:06 +0900

    mz700beep framed

--- test.c
+++ test.c
@@ -3,23 +3,60 @@
 #include <string.h>
 #include <mz700beep.h>
 
-struct note {
+typedef struct notetype {
     int length;
-    int curent_index;
     int pitch;
+    int state;
+    int current_index;
+}note;
+
+typedef struct parttype {
+    int note_index;
+    int end;
+    int note_length;
+    note notes[5];    
+}part;
+
+part beep_part1 = {0,0,5,
+    {
+        {50,3416,0,0},
+        {100,3044,0,0},
+        {50,2712,0,0},
+        {100,2564,0,0},
+        {50,2283,0,0}
+    }
 };
+
+void mz700_beep_frame(part *part1);
 
 void main(void) {
 
     mz700_beep_init();
-    uint16_t play_pitch_array[10] = {3416,3044,2712,2564,2283,3416,3044,2712,2564,2283}; //O4CDEFG
-    for (int i = 0; i < 10; i++) {
-        uint16_t play_pitch = play_pitch_array[i];
-        mz700_beep_on(play_pitch);
-        printf("\nSOUND");
+
+    part *part1 = &beep_part1;
+    uint16_t play_pitch;
+    while(part1->end==0) {
+        mz700_beep_frame(part1);
         csleep(1);
     }
-     mz700_beep_off();
+    mz700_beep_off();
+}
 
+void mz700_beep_frame(part *part1) {
+    uint16_t play_pitch;
+    note *note1 = &part1->notes[part1->note_index];
+    if (note1->state == 0 ) {
+        play_pitch = note1->pitch;
+        mz700_beep_on(play_pitch);
+        printf("\nSOUND");
+        note1->state = 1;
+    }
+    note1->current_index++;
+    if (note1->current_index >= note1->length) {
+        part1->note_index++;
+        if (part1->note_index>=part1->note_length) {
+            part1->end = 1;
+        }
+    }
 }
 
